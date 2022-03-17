a7330c982dd270a65d0cc82f01850726961f21c1
Payacan08 <fco.payacan08@gmail.com>
2022-03-16 23:30:55 -0300

    js compra

--- js/apps.js
+++ js/apps.js
@@ -1,127 +1,231 @@
-class Venta{
-    constructor(nombre,apellido,direccion){
-        this.cliente = new Cliente(nombre,apellido,direccion);
-        this.producto = [];
-        this.entrega;
-        //this.id = [];
-    }
-    
-    setProducto(nombre,cantidad){
-        let prod = new Productos();
-        switch (nombre) {
-            case "ind":
-                prod.individual(cantidad);
-                this.producto.push(prod);
-                break;
-            case "dob":
-                prod.doble(cantidad)
-                this.producto.push(prod);
-                break;
-            case "pack":
-                prod.pack4(cantidad)
-                this.producto.push(prod);
-                break;
-            case "packtaza":
-                prod.pack4Taza(cantidad)
-                this.producto.push(prod);
-                break;    
-        }
-    }
+//API
+const URLGET = "https://mindicador.cl/api"
 
-    setRetiro(){
-        this.entrega="Retiro";
-    }
-    
-    setDelivery(){
-        this.entrega="Delivery";
-    }
-
-    getTotal(){
-        let total=0;
-        for (let i = 0; i < this.producto.length; i++) {
-            total += this.producto[i].precio*this.producto[i].cantidad;            
-        }
-        return total;
-    }
-
-    getLastProducto(){
-        var i = this.producto.length;
-        var element = this.producto[i-1];
-        let mensaje=`<td><button class="btn btn-color1 btn-sm active text-color2" id="del${i-1}">X</button></td><td>${element.nombre}</td><td>${element.precio}</td><td>${element.cantidad}</td><td>${element.precio*element.cantidad}</td>`;
-        return mensaje;
-    }
-
-    getLastProductoID(){
-        var i = this.producto.length-1;
-        this.id.push(`b${i}`);
-        return i;
-    }
-
-}
-
+//Clases 
 class Cliente{
-    constructor(nombre,apellido,direccion){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.direccion = direccion;
-    }
-}
-
-class Productos{
     constructor(){
         this.nombre;
-        this.precio;
-        this.cantidad;
+        this.apellido;
+        this.celular;
+        this.entrega;
+        this.direccion;
     }
 
-    individual(cantidad){
-        this.nombre = "Individual";
-        this.precio = 1500;
-        this.cantidad = cantidad;
+    save(){
+        this.nombre = $("#nombre").val();
+        this.apellido = $("#apellido").val();
+        this.celular = $("#numero").val();
+        if ($("#radioDely1").prop("checked")) {
+            this.entrega = "retiro";
+        }else if($("#radioDely2").prop("checked")){
+            this.entrega = "delivery";
+        }
+        this.direccion = $("#direccion").val();
     }
 
-    doble(cantidad){
-        this.nombre = "Doble";
-        this.precio = 2500;
-        this.cantidad = cantidad;
+    addJSON(JSON){
+        this.nombre = JSON.nombre;
+        this.apellido = JSON.apellido; 
+        this.celular = JSON.celular; 
+        this.entrega = JSON.entrega; 
+        this.direccion = JSON.direccion; 
     }
 
-    pack4(cantidad){
-        this.nombre = "Pack de 4";
-        this.precio = 5000;
-        this.cantidad = cantidad;
+    setHtml(){
+        $("#nombre").attr("value",`${this.nombre}`);
+        $("#apellido").attr("value",`${this.apellido}`);
+        $("#numero").attr("value",`${this.celular}`);
+        if (this.entrega == "retiro") {
+            $("#radioDely1").attr("checked","checked");
+            $("#divDireccion").hide();
+        } else if(this.entrega == "delivery") {
+            $("#radioDely2").attr("checked","checked");
+            $("#divDireccion").show();
+        }
+        $("#direccion").attr("value",`${this.direccion}`);
     }
-
-    pack4Taza(cantidad){
-        this.nombre = "Pack de 4 mas taza";
-        this.precio = 6500;
-        this.cantidad = cantidad; 
-    }
-
 }
 
-function delProducto(i) {   
-    usuario.id[i] = document.getElementById(`del${i}`);
-    usuario.id[i].innerHTML = "hola";
-    usuario.id[i].addEventListener("click",()=>{
-        listaFila[i].remove();
-    });    
+class Carro{
+    constructor(){
+        this.carro = [];
+    }
+
+    addProducto(i){
+        addProducto : {
+            for (const prod of this.carro) {
+                if (prod.nombre === this.add(i).nombre){
+                    prod.cantidad += 1;   
+                    break addProducto;
+                    }
+                }
+            this.carro.push(this.add(i));
+        }    
+    }   
+
+    add(i){
+        switch (i) {
+            case 1:
+                return {id:1,nombre:"Individual",precio:1500,cantidad:1};
+                break;
+            case 2:
+                return {id:2,nombre:"Doble",precio:2500,cantidad:1};
+                break;
+            case 3:
+                return {id:3,nombre:"Pack de 4",precio:5000,cantidad:1};
+                break;
+            case 4:
+                return {id:4,nombre:"Pack de 4 mas taza",precio:6500,cantidad:1};
+                break;
+        }       
+    }
+
+    addJSON(JSON){
+        this.carro = JSON;
+        
+    }
+
+    delProducto(i){
+        addProducto : {
+            for (const prod of this.carro) {                
+                if (prod.nombre === this.add(i).nombre){
+                    prod.cantidad -= 1;
+                    if (prod.cantidad == 0){
+                        this.carro.splice(this.carro.indexOf(prod),1);
+                    }   
+                    break addProducto;
+                    }
+                }
+        }   
+    }
+
+    setHtml(entrega,moneda){
+        let mens = ``;
+        var dely = 2000;
+        for (const prod of this.carro){
+            mens += `<tr><td><button type="button" class="btn btn-color3" id="delBtn${prod.id}">-</button></td>
+                        <td>${prod.nombre}</td>
+                        <td>${Math.round((prod.precio/moneda)*100)/100}</td>
+                        <td>${prod.cantidad}</td>
+                        <td>${Math.round(((prod.precio*prod.cantidad)/moneda)*100)/100}</td></tr>`
+        }
+        if (entrega == "delivery"){
+            mens+=` <tr><td></td>
+                    <td>Delivery</td>
+                    <td>${Math.round((dely/moneda)*100)/100}</td>
+                    <td></td>
+                    <td>${Math.round((dely/moneda)*100)/100}</td></tr>`
+        }    
+        return mens;
+    }
+
+    getTotal(entrega,moneda){
+        let total = 0;
+        for (const prod of this.carro){
+            total += prod.precio*prod.cantidad;
+        }
+        if (entrega == "delivery"){
+            total+= 2000;
+        }
+        total = Math.round(((total/moneda)*100))/100
+        return total;
+    }
 }
 
-function addProducto(){
-    let producto = document.getElementById("producto").value;
-    let cantidad = document.getElementById("cantidad").value;
-    let total = document.getElementById("total");
-    usuario.setProducto(producto,cantidad);
-    let addProd = document.createElement("tr");
-    addProd.innerHTML = usuario.getLastProducto();
-    tBody.appendChild(addProd);
-    total.innerHTML = `${usuario.getTotal()}`;
-    //delProducto(usuario.getLastProductoID());
+
+//Guardar en LocalStorage y Actualizar informacion en pantalla
+function actualizar() {
+    localStorage.setItem('carro', JSON.stringify(carro.carro));
+    localStorage.setItem('cliente',JSON.stringify(cliente));
+    localStorage.setItem('moneda',`${$("#moneda").val()}`)
+    $("#tBody").empty().append(carro.setHtml(cliente.entrega,moneda));
+    $("#total").empty().append(carro.getTotal(cliente.entrega,moneda));
+    eventDel();
 }
 
-let usuario= new Venta("cliente","cliente","direccion");
-let botonAgregar = document.getElementById("agregar");
-let tBody = document.getElementById("tBody");
-const listaFila = tBody.getElementsByTagName("tr"); 
-botonAgregar.addEventListener("click",addProducto);
+//Event Delete
+function eventDel() {
+    $("#delBtn1").click(()=>{
+        carro.delProducto(1);
+        actualizar();
+    });
+    $("#delBtn2").click(()=>{
+        carro.delProducto(2);
+        actualizar();
+    });
+    $("#delBtn3").click(()=>{
+        carro.delProducto(3);
+        actualizar();
+    });
+    $("#delBtn4").click(()=>{
+        carro.delProducto(4);
+        actualizar();               
+    });
+}
+
+//Event Add
+$(document).ready(function () {
+    $("#ind").click(function () {
+        carro.addProducto(1);
+        actualizar();    
+    });
+    $("#dob").click(function () {
+        carro.addProducto(2);
+        actualizar();
+    });
+    $("#pack").click(function () {
+        carro.addProducto(3);
+        actualizar();
+    });
+    $("#pack4").click(function () {
+        carro.addProducto(4);
+        actualizar();
+    });
+    $("#save").click(function(){
+        cliente.save();
+        actualizar();
+    })
+    $("#radioDely1").click(function(){
+        $("#divDireccion").fadeOut("slow");
+    })
+    $("#radioDely2").click(function(){
+        $("#divDireccion").fadeIn("slow");
+    })
+    $("#moneda").change(function(){
+        $.get(URLGET,function(resp,estado){
+            if (estado === "success"){
+                let datos=resp;
+                moneda = 1;
+                if ($("#moneda").val()==="dolar") {
+                    moneda = datos.dolar.valor;
+                } else if($("#moneda").val()==="euro"){
+                    moneda = datos.euro.valor;
+                }
+            }
+            actualizar();
+        })
+    })
+});
+
+//inicializar
+let cliente = new Cliente();
+let carro = new Carro();
+let moneda = 1;
+
+if(localStorage.getItem("carro")!=null){
+    let local = localStorage.getItem("carro");
+    carro.addJSON(JSON.parse(local));
+    if(localStorage.getItem("cliente")!=null){
+        let localCliente = localStorage.getItem("cliente");
+        cliente.addJSON(JSON.parse(localCliente));
+        cliente.setHtml();
+    }
+    actualizar();
+}else{
+    if(localStorage.getItem("cliente")!=null){
+        let localCliente = localStorage.getItem("cliente");
+        cliente.addJSON(JSON.parse(localCliente));
+        cliente.setHtml();
+    }
+};
+
